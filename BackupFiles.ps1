bfc8458dd6ffb5c0e6923a0f9a747e1951180e35
Hayato Iriumi <hiriumi@gmail.com>
2018-04-05 09:59:56 -0700

    saving

---
+++ BackupFiles.ps1
@@ -0,0 +1,9 @@
+# Written by Hayato Iriumi for Pan Pacific Dentistry (4/5/2018)
+# hiriumi@gmail.com
+
+$SourceDirs = "C:\Users\hiriu\Dropbox\カメラアップロード","hoge"
+
+ForEach ($SourceDir In $SourceDirs)
+{
+	Write-Host $SourceDir
+}
