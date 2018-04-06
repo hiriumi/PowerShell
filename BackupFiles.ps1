72a6d6d9bd33d3af1779291669623c26b7b920a5
Hayato Iriumi <hiriumi@gmail.com>
2018-04-05 23:10:37 -0700

    saving

--- BackupFiles.ps1
+++ BackupFiles.ps1
@@ -7,28 +7,28 @@ $RetentionDays = 120
 $BackupRootDir = "F:\test_backup"
 $BackupTargetDir = Join-Path -Path $BackupRootDir -ChildPath $DateTimeNow.ToString("yyyy-MM-dd-HH-mm-ss")
 $SourceDirs = "C:\test1","C:\test2"
+$LogFilePath = Join-Path -Path $BackupTargetDir -ChildPath "log.txt"
 
-Write-Host "Backing up to $BackupTargetDir..."
+Add-Content -Path $LogFilePath -Value "Backing up to $BackupTargetDir..."
 
 If (!(Test-Path -Path $BackupTargetDir))
 {
-	Write-Host "Creating a new directory $BackupTargetDir..."
+	Add-Content -Path $LogFilePath -Value "Creating a new directory $BackupTargetDir..."
 	New-Item -ItemType Directory -Force -Path $BackupTargetDir
 }
-
-
 
 ForEach ($SourceDir In $SourceDirs)
 {
 	If (Test-Path -Path $SourceDir)
 	{
-		Write-Host "Backing up $SourceDir to $BackupTargetDir..."
+		Add-Content -Path $LogFilePath -Value "Backing up $SourceDir to $BackupTargetDir..."
 		Copy-Item -Path $SourceDir -Filter "*.*" -Recurse -Destination $BackupTargetDir -Container
-		Write-Host "Backed up $SourceDir successfully."
+		Add-Content -Path $LogFilePath -Value "Backed up $SourceDir successfully."
 	}
 	Else
 	{
 		Write-Warning "The source directory path `"$SourceDir`" does not exist."
+		Add-Content -Path $LogFilePath -Value "The source directory path `"$SourceDir`" does not exist."
 	}
 }
 
@@ -43,4 +43,4 @@ ForEach ($Dir In $AllDirs)
 	}
 }
 
-Write-Host "Backup completed successfully at $([datetime]::Now)"
+Add-Content -Path $LogFilePath -Value "Backup completed successfully at $([datetime]::Now)"
